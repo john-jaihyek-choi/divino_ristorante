9a361fe0e2968300d66b52f946b4723d41ad1aa3
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-08-31 23:58:51 -0700

    completed header grid-layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/header/header";
+// import ExternalFooter from "./components/externalFooter/externalFooter";
 import "./App.css";
-import "./components/externalFooter/externalFooter";
-import ExternalFooter from "./components/externalFooter/externalFooter";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Route exact path="/" component={Header} />
+        <Header />
+        {/* <Route exact path="/" component={ExternalFooter} /> */}
       </BrowserRouter>
     </>
   );
