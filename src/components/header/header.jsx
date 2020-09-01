b69559518b507e2a37559d1c7e0ffd593b9c1626
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-09-01 02:34:03 -0700

    changed id on header component for order now h6 element

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./header.module.css"
+import styles from "./header.module.css"
 
 const Header = props => {
     return (
         <header className="container">
             <div className="container h-100">
                 <div className="row h-100">
-                    <h6 id="orderNow" className='col-4 align-self-center text-white text-center'><u>Order Now</u></h6>
-                    <h2 id="divinoMain" className='col-4 align-self-center text-white text-center'>Divino</h2>
+                    <h6 id={styles.orderNow} className='col-4 align-self-center text-white text-center'><u>Order Now</u></h6>
+                    <h2 className='col-4 align-self-center text-white text-center'>Divino</h2>
                     <span className='col-4 align-self-center text-white text-center h-2'><i className="fas fa-bars fa-2x"></i></span>
                 </div>
             </div>
