a6b2225de925e9f22a1160c5452b10a853275eb1
MicrosoftApple <brian930429@gmail.com>
2020-09-01 00:03:21 -0500

    Centered items inside footer. Set up style.css for footer.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import './components/externalFooter/externalFooter';
+import ExternalFooter from './components/externalFooter/externalFooter';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <ExternalFooter/>
     </div>
   );
 }
