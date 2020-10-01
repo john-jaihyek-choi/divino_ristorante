8bc1d0656e2bca2a559c86045522417e52f5accf
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-10-01 12:47:33 -0700

    set up background image and a text button to navigate to menu

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Main from "./main/main";
 import Contact from "./contact/contact"
+import Menu from "./menu/menu";
+import Reservation from "./reservation/reservation"
 import "./landing.module.css";
 import styles from "./landing.module.css";
 
@@ -9,6 +11,8 @@ function Landing() {
     <section id={styles.landing} className="container-fluid px-0">
       <Main />
       <Contact />
+      <Menu />
+      <Reservation />
     </section>
   );
 }
